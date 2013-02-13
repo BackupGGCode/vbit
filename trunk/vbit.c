b52c5c34206e78346beabe7dee401fedbfd3e5fa
peterk.vt80@gmail.com <peterk.vt80@gmail.com@7355d328-be4d-e316-54f9-dafef74ab5c5>
2013-02-13 00:31:03 +0000

    Fixed language bits and C11. I suspect that the other C bits may be wrongly aligned or reversed.

--- trunk/vbit.c
+++ trunk/vbit.c
@@ -7,7 +7,7 @@
  * Permission to use, copy, modify, and distribute this software
  * and its documentation for any purpose and without fee is hereby
  * granted, provided that the above copyright notice appear in all
- * copies and that both that the copyright notice itand this
+ * copies and that both that the copyright notice it and this
  * permission notice and warranty disclaimer appear in supporting
  * documentation, and that the name of the author not be used in
  * advertising or publicity pertaining to distribution of the
